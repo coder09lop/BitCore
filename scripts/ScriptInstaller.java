31464609356565718f6cb4ece7d3730639d3b5cc
coder09lop <omargrini460@gmail.com>
2024-08-28 15:15:06 +0200

    scripts created

---
+++ scripts/ScriptInstaller.java
@@ -0,0 +1,10 @@
+package scripts;
+
+public class ScriptInstaller {
+
+    public void installScript(String name, String version, String location) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'installScript'");
+    }
+
+}
