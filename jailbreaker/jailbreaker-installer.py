94a3739fd0a659dff911eaadc252dfe9c6a04b12
coder09lop <omargrini460@gmail.com>
2024-08-29 17:33:03 +0200

    Update jailbreaker-installer.py

--- jailbreaker/jailbreaker-installer.py
+++ jailbreaker/jailbreaker-installer.py
@@ -1,39 +1,31 @@
 import os
 import random
-
-
+import subprocess
+#inizia
 def JailBreaker():
     print("Welcome to the jailbreaker mod!")
     print("NOTE. the jailbreaker mod is a mod 100% legal for linux this is created from us don't use it for illegal things")
-    print("what you want to do?")
-    print("1. jailbreak version in BITCORE")
-    print("2. jailbreak linux os")
-    print("3. jailbreak windows os")
-    print("4. jailbreak MACOS(not working)")
-    print("5. exit")
-    print("1-5")
-    choice = input("Enter your choice: ")
-    if choice == "1":
-        print("BITCORE jailbreaker")
-        os.system("sudo apt update")
-        os.system("sudo apt install qemu")
-        os.system("sudo apt install binfmt-support")
-        os.system("sudo apt install libglib2.0-dev")
-        os.system("sudo apt install libglib2.0-dev")
-    elif choice == "2":
-        os.system("sudo apt update")
-        os.system("sudo apt install qemu")
-        os.system("sudo apt install binfmt-support")
-        os.system("sudo apt install libglib2.0-dev")
-    elif choice == "3":
-        os.system("sudo apt update")
-        os.system("sudo apt install qemu")
-        os.system("sudo apt install binfmt-support")
-        os.system("sudo apt install libglib2.0-dev")
-    elif choice == "4":
-        print("MACOS jailbreaker")
-    elif choice == "5":
-        print("Exiting...")
+    print("what to jailbreak?")
+    print("1. the bitcore")
+    print("2. exit")
+    print("1-2")
+    jailbreak = input("choose a number: ")
+    if jailbreak == "1":
+        subprocess.run(["sudo", "apt", "update"])
+        subprocess.run(["sudo", "apt", "install", "visual-studio-code"])
+        subprocess.run(["sudo", "apt", "install", "git"])
+        subprocess.run(["sudo", "apt", "install", "python3"])
+        subprocess.run(["sudo", "apt", "install", "python3-pip"])
+        subprocess.run(["sudo", "pip3", "install", "pyautogui"])
+        #jdk
+        subprocess.run(["sudo", "apt", "install", "default-jdk"])
+        #node
+        subprocess.run(["sudo", "apt", "install", "nodejs"])
+        #npm
+        subprocess.run(["sudo", "apt", "install", "npm"])
+        #java
+        subprocess.run(["sudo", "apt", "install", "openjdk-8-jdk"])
+    elif jailbreak == "2":
+        print("goodbye!")
     else:
-        print("not a valid option")
-    #finisch
+        print("invalid choice!")
