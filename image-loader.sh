e63042dec2b62a717f8a21cd19cc59025eb71a14
coder09lop <omargrini460@gmail.com>
2024-08-29 12:57:44 +0200

    Update image-loader.sh

--- image-loader.sh
+++ image-loader.sh
@@ -3,10 +3,11 @@ echo "image loader"
 echo "what image you loaded"
 #if statement
 if [ "$1" == "win11" ]; then
-echo "you loaded win11"
+    echo "you loaded win11"
 elif [ "$1" == "win10" ]; then
-echo "you loaded win10"
+    echo "you loaded win10"
 elif ["$1" == "macos"]; then
-echo "image not loaded"
+    echo "image not loaded"
 else
-echo "you loaded something else"
+    echo "you loaded something else"
+fi
