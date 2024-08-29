da8ee38f9b547a24b6f439c6ffaae8003b32b5bc
coder09lop <omargrini460@gmail.com>
2024-08-29 14:51:25 +0200

    ristrutturated installer.java

--- installer.java
+++ installer.java
@@ -1,81 +1,68 @@
 import java.util.Scanner;
-import java.util.regex.Matcher;
-@SuppressWarnings("unused")
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
-//inizio programma
+
+
+
+
+
+
 
 public class installer {
-   public static void main(String[] args) {
-    // Create a new Scanner object
-    Scanner scanner = new Scanner(System.in);
-    System.out.println("benvenuto nella hacker box ");
-    System.out.println("inserisci il tuo nome");
-    String nome = scanner.nextLine();
-    
-    switch(nome) {
-        case "hacker":
-        System.out.println("benvenuto hacker");
-        break;
-        case "oms":
-        System.out.println("benvenuto oms");
-        System.out.println("inserisci password");
-        String oms = scanner.nextLine();
+    public static void main(String[] args) {
+        // Create a new instance of the installer
+        System.out.println("welcome to the BitCore installer");
+        System.out.println("Please select an option:");
+        System.out.println("1. Install BitCore");
+        System.out.println("2. Uninstall BitCore");
+        System.out.println("3. Update BitCore");
+        System.out.println("4. Exit");
+        @SuppressWarnings("resource")
+        Scanner scanner = new Scanner(System.in);
+        int choice = scanner.nextInt();
+            switch (choice) {
+                case 1 -> {
+                    System.out.println("Installing BitCore...");
+                    System.out.println("Please wait...");
+                    SimulateLoading();
+                    System.out.println("Installation complete!");
+                    break;
+            }
+                case 2 -> {
+                    System.out.println("Uninstalling BitCore...");
+                    System.out.println("Please wait...");
+                    SimulateLoading();
+                    break;
+                }
+                case 3 -> {
+                    System.out.println("Updating BitCore...");
+                    System.out.println("Please wait...");
+                    SimulateLoading();
+                    break;
+                }
+                case 4 -> {
+                    System.out.println("Exiting...");
+                    System.exit(0);
+                    break;
+                }
+            }
         
-        if (oms == "password"); {
-            System.out.println("acesso consentito");
-            System.out.println("benvenuto nell pannello di controllo");
-            System.out.println("inserisci comando");
-        }
-        break;
-        default:
-        System.out.println("benvenuto");
-        System.out.println(nome);
-        System.out.println("scegli una password");
-        String password = scanner.nextLine();
-        
-        switch(password) {
-            case "password":
-            System.out.println("password molto prevedibile");
-            break;
-            case "1234":
-            System.out.println("password molto debole");
-            break;
-            default:
-            System.out.println("password accettabile");
-            System.out.println("ok possiamo continuare");
-            //creami il disco
-            System.out.println("creazione disco in corso");
-            simulateLoading();
-            System.out.println("disco creato");
-            //installazione del sistema operativo
-            System.out.println("installazione sistema operativo in corso");
-            simulateLoading();
-            System.out.println("sistema operativo installato");
-            //installazione dei driver
-            System.out.println("installazione driver in corso");
-            simulateLoading();
-            //installazione addon
-            System.out.println("installazione addon in corso");
-            simulateLoading();
-            System.out.println("premi invio per usire");
-            scanner.nextLine();
-            scanner.close();
-            break;
-        }
-        
-        scanner.close();
+
     }
-    
-   }
-   private static void simulateLoading() {
-    for (int i = 0; i < 10; i++) {
-        System.out.print("\rLoading: " + i + "%");
+    private static void SimulateLoading() {
+        ScheduledExecutorService executor = Executors.newSingleThreadScheduledExecutor();
+        @SuppressWarnings("unused")
+        Runnable loadingTask = () -> System.out.println("Loading...");
+        executor.scheduleAtFixedRate(loadingTask, 0, 1, TimeUnit.SECONDS);
         try {
-            Thread.sleep(100);
-            } catch (InterruptedException e) {
-                Thread.currentThread().interrupt();
-                }
-                }
-                System.out.println();
-   }
-}
+            
+                Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    }
+                executor.shutdown();
+            
+
+        }
+    }
